c48b3a53f3f72f8340f26d6a962127d9407022e2
Christopher Akatsuka <akatsuka@sas.upenn.edu>
2014-04-28 17:17:06 -0400

    Added points, scoring, time, and pause functionality.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class WhosThatPokemon(wx.Frame):
         self.GetSizer().Add(item=self.png, proportion=1) 
         self.score = 0
         self.time = 0
+        self.points = 10
         self.pause = False
         self.Timer = wx.Timer(self)
         self.Timer.Start(1000)
@@ -33,19 +34,34 @@ class WhosThatPokemon(wx.Frame):
 
     def Enter(self, event):
         key = event.GetEventObject().GetValue()
-        
+        if(key == 'Pikachu' and not self.pause):
+            self.score += self.points
+            self.score_button.SetLabel("Score: " + str(self.score))
+            self.points = 10
+
+
     def CreateMenuButtons(self):
-        gs = wx.GridSizer(1,2)
-        quit = wx.Button(parent=self, label='Quit')
+        gs = wx.GridSizer(3,2)
+
         self.timer_button = wx.Button(parent=self, label='Time: ' + str(self.time))
+        pause = wx.Button(parent=self, label='Pause')
+        self.score_button = wx.Button(parent=self, label='Score: ' + str(self.score))
+        self.points_button = wx.Button(parent=self, label='Points: ' + str(self.points))
+        quit = wx.Button(parent=self, label='Quit')
+        restart = wx.Button(parent=self, label='Restart')
 
-        gs.Add(item=quit, flag=wx.EXPAND)
         gs.Add(item=self.timer_button, flag=wx.EXPAND)
-
-        self.Bind(event=wx.EVT_BUTTON, handler=self.Quit, source=quit)
+        gs.Add(item=pause, flag=wx.EXPAND)
+        gs.Add(item=self.score_button, flag=wx.EXPAND)
+        gs.Add(item=self.points_button, flag=wx.EXPAND)
+        gs.Add(item=quit, flag=wx.EXPAND)
+        gs.Add(item=restart, flag=wx.EXPAND)
+        
+        
         self.Bind(wx.EVT_TIMER, self.update_timer, self.Timer)
-        self.Bind(event=wx.EVT_BUTTON, handler=self.Pause, source=self.timer_button)
-
+        self.Bind(event=wx.EVT_BUTTON, handler=self.Pause, source=pause)
+        self.Bind(event=wx.EVT_BUTTON, handler=self.Restart, source=restart)
+        self.Bind(event=wx.EVT_BUTTON, handler=self.Quit, source=quit)
 
         self.GetSizer().Add(item=gs, flag=wx.EXPAND)
     
@@ -55,10 +71,23 @@ class WhosThatPokemon(wx.Frame):
     def update_timer(self, event):
         if(self.pause == False):
             self.time += 1
-            self.timer_button.SetLabel("Time: " + str(self.time))
+            if (self.points > 0):
+                self.points -= 0.5
+        self.timer_button.SetLabel("Time: " + str(self.time))
+        self.points_button.SetLabel("Points: " + str(self.points))
+        
     def Pause(self, event):
-        self.pause = not (self.pause)        
-             
+        self.pause = not (self.pause)    
+
+    def Restart(self, event):
+        self.time = 0
+        self.score = 0
+        self.points = 10
+        self.points_button.SetLabel("Points: " + str(self.points))
+        self.score_button.SetLabel("Score: " + str(self.score))
+        self.timer_button.SetLabel("Time: " + str(self.time))
+
+
 
 def main():
     app = wx.App()
