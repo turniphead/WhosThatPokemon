c7943cadf66c0dfc226ae587a27a8113ce87adc4
turniphead <darkwinter2424@gmail.com>
2014-05-09 16:20:23 -0400

    fixed tab errors

--- open.py
+++ open.py
@@ -2,30 +2,25 @@ import wx
 import os
 
 class Instructions(wx.Frame):
-    
-    def __init__(self, *args, **keywords):
+	def __init__(self, *args, **keywords):
+		wx.Frame.__init__(self, *args, **keywords)
+		fi = open('instructions.txt','r')
+		self.instr = fi.read()
+		fi.close()
+		sizer = wx.BoxSizer(orient=wx.VERTICAL)
+		self.SetSizer(sizer)
+		self.MakeText()
+		self.MakeButton()
 
-        wx.Frame.__init__(self, *args, **keywords)
-
-        fi = open('instructions.txt','r')
-        instr = fi.read()
-        fi.close()
-        sizer = wx.BoxSizer(orient=wx.VERTICAL)
-        self.SetSizer(sizer)
-
-        self.MakeText()
-        self.MakeButton()
-
-    def MakeText(self):
-    	self.text = wx.TextCtrl(self, value=instr)
-	    self.text.SetEditable(False)
-	    self.GetSizer().Add(item=self.text, flag=wx.EXPAND)
-        
-        
+	def MakeText(self):
+		self.text = wx.TextCtrl(self, value=self.instr)
+		self.text.SetEditable(False)
+		self.GetSizer().Add(item=self.text, flag=wx.EXPAND)
 
 	def MakeButton(self):
-		self.button = wx.Button(parent=self, label="I'm Reaedy!")
+		self.button = wx.Button(parent=self, label="I'm Ready!")
 		self.Bind(event=wx.EVT_BUTTON, handler=self.GoToGame, source=self.button)
+		self.GetSizer().Add(item=self.button, flag=wx.EXPAND)
 
 	def GoToGame(self,event):
 		self.Destroy()
