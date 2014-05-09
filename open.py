ea8d51c88f58817ed3eaa65895fdfeb8993bb2c4
Christopher Akatsuka <akatsuka@sas.upenn.edu>
2014-05-09 18:11:15 -0400

    Added comments

--- open.py
+++ open.py
@@ -1,30 +1,44 @@
+''' The introduction screen that allows the user to either start the game or quit. '''
 import wx
 import game
+
 class Instructions(wx.Frame):
+
     def __init__(self, *args, **keywords):
+        #Create frame and sizer 
         wx.Frame.__init__(self, *args, **keywords)
         sizer = wx.BoxSizer(orient=wx.VERTICAL)
         self.SetSizer(sizer)
+
+        #Background image
         Background = wx.StaticBitmap(self, -1, wx.Bitmap('Intro.png', wx.BITMAP_TYPE_ANY))
         sizer.Add(item=Background, flag=wx.EXPAND)
         self.MakeButtons()
         sizer.Fit(self)
+
     def MakeButtons(self):
+        #Create grid sizer for buttons
         gs = wx.GridSizer(1,2)
 
+        #Create buttons
         start = wx.Button(parent=self, label="I'm ready!")
         quit = wx.Button(parent=self, label="I'm not ready yet!")
 
+        #Add buttons to grid sizer
         gs.Add(item=start, flag=wx.EXPAND)
         gs.Add(item=quit, flag=wx.EXPAND)
 
+        #Bind button pushes to correct method calls
         self.Bind(event=wx.EVT_BUTTON, handler=self.GoToGame, source=start)
         self.Bind(event=wx.EVT_BUTTON, handler=self.Quit, source=quit)
 
+        #Add grid sizer to the sizer
         self.GetSizer().Add(item=gs, flag=wx.EXPAND)
     
     def GoToGame(self, event):
         self.Destroy()
+
+        #Calls the main method of game.py
         game.main()
         
 
